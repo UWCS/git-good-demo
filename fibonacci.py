1e88108ddc88603e94ab441702e8650e12d09284
uwcs <exec@uwcs.co.uk>
2024-09-10 19:51:58 +0100

    added input validation

--- fibonacci.py
+++ fibonacci.py
@@ -4,7 +4,12 @@ a = 0
 b = 1
 
 output = f"{a}, {b}"
-terms = int(sys.argv[1])
+
+try:
+    terms = int(sys.argv[1])
+except:
+    print("Not a number, defaulting to 10")
+    terms = 10
 
 for i in range(3, terms + 1):
     a, b = b, a + b
